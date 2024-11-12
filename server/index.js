7a5e91d1ee6cbf7b0faadf1b90a8b7fa3e73e0ee
DESKTOP-P8ALHH3\USER <dukyong99@naver.com>
2024-11-12 23:46:13 +0900

    qew

--- server/index.js
+++ server/index.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const app = express();
 const http = require("http");
-const cors = require("cors");
 const { Server } = require("socket.io");
+const cors = require("cors");
+
 app.use(cors());
-
 const server = http.createServer(app);
-
-const port = 4000;
 
 const io = new Server(server, {
   cors: {
@@ -17,25 +15,13 @@ const io = new Server(server, {
 });
 
 io.on("connection", (socket) => {
-
-  socket.on("join_room", (data) => {
-    socket.join(data.room);
-    console.log(`${data.username}유저가 ${data.room}번 방에 입장했습니다`);
-    let noti = {
-      message:`${data.username} 유저가 방에 입장했습니다`,
-      author:'알림'
-    }
-    socket.to(data.room).emit('receive_message', noti);
-  });
+  console.log(`User Connected: ${socket.id}`);
 
   socket.on("send_message", (data) => {
-    console.log(data)
-    socket.to(data.room).emit("receive_message", data);
-  });
-
-  socket.on("disconnect", () => {
-    console.log(`${socket.id}가 접속을 끊었습니다`);
+    socket.broadcast.emit("receive_message", data);
   });
 });
 
-server.listen(port, () => console.log(`server running on port ${port}`));
+server.listen(4000, () => {
+  console.log("SERVER IS RUNNING");
+});
